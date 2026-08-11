c7f2ddbcb3b778e93795ac8b4210b4951fc5d7ce
Dongsam <vvddalfmr@gmail.com>
2026-08-11 13:23:27 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 197 ms, Memory: 62,720 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
@@ -1,32 +1,37 @@
 import heapq
- 
+
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
- 
+
 def find_route():
- 
-    pq = []
+
     dist = [[-1] * N for _ in range(N)]
-    heapq.heappush(pq, (0, 0, 0))
+    pq = []
     dist[0][0] = 0
+    heapq.heappush(pq, (0, 0, 0))
     while pq:
-        cost, x, y = heapq.heappop(pq)
+        cur_dist, x, y = heapq.heappop(pq)
+
+        if dist[x][y] != -1 and cur_dist > dist[x][y]:
+            continue
+
         for idx in range(4):
             nx, ny = x + dx[idx], y + dy[idx]
             if 0 <= nx < N and 0 <= ny < N:
-                new_cost = cost + road[nx][ny]
-                if dist[nx][ny] == -1 or dist[nx][ny] > new_cost:
-                    dist[nx][ny] = new_cost
-                    heapq.heappush(pq, (new_cost, nx, ny))
- 
+                new_dist = cur_dist + board[nx][ny]
+                if dist[nx][ny] == -1 or dist[nx][ny] > new_dist:
+                    dist[nx][ny] = new_dist
+                    heapq.heappush(pq, (new_dist, nx, ny))
+
     return dist[N-1][N-1]
- 
- 
+
 T = int(input())
- 
+
 for tc in range(1, T+1):
- 
+
     N = int(input())
-    road = [list(map(int, input().strip())) for _ in range(N)]
- 
-    print(f'#{tc} {find_route()}')
+    board = [list(map(int, input().strip())) for _ in range(N)]
+
+    result = find_route()
+
+    print(f'#{tc} {result}')
